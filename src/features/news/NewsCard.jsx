593ba70f426cf3eddfdf1647824e357a6521886e
dennis-dentrix <denkyusya@gmail.com>
2023-11-08 13:31:05 +0300

    news source

--- src/features/news/NewsCard.jsx
+++ src/features/news/NewsCard.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export function NewsCard({ news }) {
   return (
-    <div className=" flex flex-col w-64 sm:w-60 rounded-md">
+    <div className=" flex flex-col w-72 sm:w-60 rounded-md">
       <img src={news.image} alt={news.title} className="w-full" />
       <div className="bg-white px-2 rounded-b-md py-2 border border-black">
         <Link className="font-bold text-lg tracking-wide">{news.title}</Link>
-        <p className="text-black border-l pl-1 border-blue">DW news</p>
+        <p className="text-black border-l pl-1 border-blue">Read more</p>
       </div>
     </div>
   );
