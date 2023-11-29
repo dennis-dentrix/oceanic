8c77c9ef4c80b1bf2db2342d99780aea65ba4234
dennis-dentrix <denkyusya@gmail.com>
2023-11-29 09:15:59 +0300

    Resource

--- src/features/Resources/Resources.jsx
+++ src/features/Resources/Resources.jsx
@@ -36,27 +36,60 @@ export default function Resources() {
       {/* {/* <section>Section 2</section> */}
 
       <section>
-        <div className="w-1/2 px-2 py-4">
-          <video
-            controls
-            preload="none"
-            poster="./img-2.jpeg"
-            width="350"
-            aria-orientation="landscape"
-          >
-            {/* <source src="one-does-not-simply.webm" type="video/webm"/> */}
-            <source src="./vid-4.mp4" type="video/mp4" />
-          </video>
-          <div>
-            <p className="text-sm text-black tracking-wide">
-              <span className="text-grey">By: </span>
-              Alex mwendwa
-            </p>
-            <a href="/doc.pdf" target="_blank">
-              <h2 className="text-black font-bold text-lg tracking-wide">
-                Saving the last of mangroove
-              </h2>
-            </a>
+        <div className="space-y-3">
+          <h2 className="text-lg font-bold capitalize underline">
+            Learn by watching
+          </h2>
+
+          <div className="flex flex-col md:flex-row gap-6">
+            <div className="">
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/rM6txLtoaoc`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+
+            <div>
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/ttcJMNlKif4`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col md:flex-row gap-6">
+            <div className="">
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/nwyqp8KFGiI`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
+
+            <div>
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/EJW_Rkn5xZM`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+            </div>
           </div>
         </div>
       </section>
--- src/features/learning/Course.jsx
+++ src/features/learning/Course.jsx
@@ -18,63 +18,6 @@ export default function Course() {
           <ExamDrawer />
         </div>
       </div>
-
-      <div className="space-y-3">
-        <h2 className="text-lg font-bold capitalize underline">
-          Learn by watching
-        </h2>
-
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="">
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/rM6txLtoaoc`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-
-          <div>
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/ttcJMNlKif4`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="">
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/nwyqp8KFGiI`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-
-          <div>
-            <iframe
-              width="560"
-              height="315"
-              src={`https://www.youtube.com/embed/EJW_Rkn5xZM`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
